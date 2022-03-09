6b0b179273ed736883c7b0c8ee8cfa045502eda5
bayanabdo43 <bayanabdo43@yahoo.com>
2022-03-09 03:25:48 +0200

    neew

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ console.log(process.argv[2])
 
 
 const coand=process.argv[2]
-if (coand=== 'add'){
+if (coand === 'add'){
     console.log('add it')
 }
 else if (coand==='delete'){
@@ -32,9 +32,31 @@ else{
 }
 
 
+//////////////بستدعي ملف yarags
 const yargs = require('yargs')
 const { describe } = require('yargs')
 console.log(yargs.argv)
+
+
+
+
+yargs.command({
+    command:'add',
+    describe:'add note',
+    handler:()=>{
+        console.log('add note')
+    }
+})
+
+
+yargs.command({
+    command:'delete',
+    describe:'delete note',
+    handler:()=>{
+        console.log('delete note')
+    }
+})
+
 
 
 yargs.command({
@@ -47,14 +69,85 @@ yargs.command({
 
 
 
+yargs.command({
+    command:'read',
+    describe:'read note',
+    handler:()=>{
+        console.log('read note')
+    }
+})
+
+
+
+
+yargs.command({
+    command:'delete',
+    describe:'delet note',
+    builder:{
+        title:{
+            describe:'',
+            demandOption:true,
+            type:'string'
+        },
+        handler:()=>{
+            console.log('delet note')
+        }
+    }
+})
+
+
+
+
+
+yargs.command({
+    command:'read',
+    describe:'read note',
+    builder:{
+        title:{
+            describe:'read note',
+            demandOption:true,
+            type:'string'
+        },
+        handler:()=>{
+            console.log('delet note')
+        }
+    }
+})
+
+
+
+const person={
+    name:'bayan',
+    age:22
+}
+
+const personjson = JSON.stringify(person)
+console.log(personjson)
+
+//console.log(fs=require('fs'))
+fs.writeFileSync('person.json', personjson)
+
+const data = fs.readFileSync('person.json').toString
+console.log(data)
+
+const dataobject = JSON.parse(data)
+console.log(dataobject)
+
+dataobject.name='abdo'
+dataobject.age=45
+
+const newjson = JSON.stringify(dataobject)
+fs.writeFileSync('person.json',newjson)
+
 
 /*const fs = reqire('fs')
 fs.writeFileSync('notes.txt','hello')
-console.log(fs.readfilesync('note.txt').toString())*/
+console.log(fs.readfilesync('notes.txt').toString())*/
 
 
 
-const notes =require('./notes')
+
+/*const notes =require('./notes')
 yargs.command({
     command:'add',
     describe:'add note',
@@ -73,4 +166,4 @@ yargs.command({
     handler:(x)=>{
         notes.addnote(x.title,x.body)
     }
-})
+})*/
